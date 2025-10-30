e9a34f3a5bf5057df39d27da99f8380383c8ef07
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-10-30 22:29:52 +0700

    Update htop.sh

--- armv5/htop.sh
+++ armv5/htop.sh
@@ -1,5 +1,8 @@
-export CFLAGS="-O2 -pipe -march=armv5te -mfloat-abi=soft"
 #The locations of the -lncurses -ltinfo libraries for armel are located separately.
+cd /home/nguyen/usr/lib/arm-linux-gnueabi
+printf 'INPUT(libncurses.a)\n' > libncurses.so
+printf 'INPUT(libtinfo.a)\n' > libtinfo.so
+export CFLAGS="-O2 -pipe -march=armv5te -mfloat-abi=soft"
 export LDFLAGS="-L/home/nguyen/usr/lib/arm-linux-gnueabi -static -Wl,--allow-multiple-definition"
 export PPFLAGS="-I/home/nguyen/usr/include"
 export LIBS="-lncurses -ltinfo"
